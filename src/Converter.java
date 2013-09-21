3162a2814783be74638831c24342fd8aa46701cb
Andrew Mass <amass1212@gmail.com>
2013-09-21 16:15:16 -0500

    Update scanning

--- src/Converter.java
+++ src/Converter.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.swing.JOptionPane;
 import javax.swing.JProgressBar;
@@ -10,10 +12,19 @@ public class Converter {
 	private Display display;
 	private File inputFile;
 	private File outputFile;
+	private Map<String, String[]> msgdat;
 	
 	public Converter() {
 		display = new Display(this);
 		display.setVisible(true);
+		initMessageInfo();
+	}
+	
+	//msgId, [Sensor Name, Data Byte Length, Subname1?, Scalar1, ScalarUnits1, Subname2, Scalar2, ScalarUnits2]
+	public void initMessageInfo() {
+		msgdat = new HashMap<String, String[]>();
+		
+		msgdat.put("0060", new String[] {"Front Hub", "4", "Sus Pot FR", "0.012207", "mm", "Sus Pot FL", "0.012207", "mm"});
 	}
 	
 	public void setInputFile(File file) {
@@ -26,7 +37,7 @@ public class Converter {
 	
 	public String hex(int num) {
 		String temp = Integer.toHexString(num);
-		if(temp.length() < 2) temp = "0" + temp;
+		while(temp.length() < 2) temp = "0" + temp;
 		return temp;
 	}
 	
@@ -43,9 +54,39 @@ public class Converter {
 		}
 		
 		for(int i=0; i<1; i++) {
-			if(hex(data[i] + data[i+1]).equals("60")) {
-				System.out.println(hex(data[i]) + " " + hex(data[i+1]) + " " + hex(data[i+2]) + " " + hex(data[i+3]) + " " + hex(data[i+4]) + " " + hex(data[i+5]));
-				System.out.println("Front Hub - " + "Sus Pot FR: " + (Integer.parseInt(hex(data[i+2]) + hex(data[i+3]), 16)) + " Sus Pot FL: " + Integer.parseInt(hex(data[i+4]) + hex(data[i+5]), 16));
+			String msgId = hex(data[i+1]) + hex(data[i]);
+			System.out.println(msgId);
+			if(msgdat.containsKey(msgId)) {
+				String printString = null;
+				
+				if(Integer.parseInt(msgdat.get(msgId)[1]) == 0) { 
+					
+				}
+				
+				if(Integer.parseInt(msgdat.get(msgId)[1]) == 2) { 
+					
+				}
+				
+				if(Integer.parseInt(msgdat.get(msgId)[1]) == 4) { 
+					printString = msgdat.get(msgId)[0] + " - " 
+						+ msgdat.get(msgId)[2] + ": " + (Integer.parseInt(hex(data[i+2]) + hex(data[i+3]), 16) * Double.parseDouble(msgdat.get(msgId)[3])) + msgdat.get(msgId)[4] + " " 
+						+ msgdat.get(msgId)[5] + ": " + (Integer.parseInt(hex(data[i+4]) + hex(data[i+5]), 16) * Double.parseDouble(msgdat.get(msgId)[6])) + msgdat.get(msgId)[7];
+					i = i + 10;
+				}
+				
+				if(Integer.parseInt(msgdat.get(msgId)[1]) == 6) { 
+					
+				}
+				
+				if(Integer.parseInt(msgdat.get(msgId)[1]) == 8) { 
+					
+				}
+				
+				System.out.println(printString);
+				i--; // Handle increment on our own
+			} 
+			else {
+				System.out.println("Message Code Not Found");
 			}
 		}	
 	}
