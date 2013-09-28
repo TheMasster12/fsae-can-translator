1dc6ed4cdd110d68e5c62604aac1e58455be1acc
Andrew Mass <amass1212@gmail.com>
2013-09-27 19:08:37 -0500

    Fixes offset glitch for Yaw Rate.

--- src/Converter.java
+++ src/Converter.java
@@ -27,16 +27,16 @@ public class Converter {
 	
 	public void initMessageInfo() {
 		msgdat = new HashMap<String, MessageType>();
-		msgdat.put("0050", new MessageType("0050", 4, "Rear Hub", new SubMessage[] {new SubMessage("Suspension Position RL", false, 0.012207, "mm"), new SubMessage("Suspension Position RR", false, 0.0148866, "mm")}));
-		msgdat.put("0060", new MessageType("0060", 4, "Front Hub", new SubMessage[] {new SubMessage("Suspension Position FR", false, 0.012207, "mm"), new SubMessage("Suspension Position FL", false, 0.012207, "mm")}));
-		msgdat.put("0070", new MessageType("0070", 8, "Accelerometer", new SubMessage[] {new SubMessage("Yaw Rate", true, 0.005, "deg/sec"), new SubMessage("Reserved", true, 1, ""), new SubMessage("Acceleration Y Axis", true, 0.0001274, "g"), new SubMessage("Unused", true, 1, "")}));
-		msgdat.put("0080", new MessageType("0080", 8, "Accelerometer", new SubMessage[] {new SubMessage("Yaw Acceleration", true, 0.0125, "deg/sec^2"), new SubMessage("Reserved", true, 1, ""), new SubMessage("Acceleration x Axis", true, 0.0001274, "g"), new SubMessage("Unused", true, 1, "")}));
+		msgdat.put("0050", new MessageType("0050", 4, "Rear Hub", new SubMessage[] {new SubMessage("Suspension Position RL", false, false, 0.012207f, "mm"), new SubMessage("Suspension Position RR", false, false, 0.0148866f, "mm")}));
+		msgdat.put("0060", new MessageType("0060", 4, "Front Hub", new SubMessage[] {new SubMessage("Suspension Position FR", false, false, 0.012207f, "mm"), new SubMessage("Suspension Position FL", false, false, 0.012207f, "mm")}));
+		msgdat.put("0070", new MessageType("0070", 8, "Accelerometer", new SubMessage[] {new SubMessage("Yaw Rate", true, false, 0.005f, "deg/sec"), new SubMessage("Reserved", true, false, 1, ""), new SubMessage("Acceleration Y Axis", true, false, 0.0001274f, "g"), new SubMessage("Unused", true, false, 1, "")}));
+		msgdat.put("0080", new MessageType("0080", 8, "Accelerometer", new SubMessage[] {new SubMessage("Yaw Acceleration", true, false, 0.0125f, "deg/sec^2"), new SubMessage("Reserved", true, false, 1, ""), new SubMessage("Acceleration x Axis", true, false, 0.0001274f, "g"), new SubMessage("Unused", true, false, 1, "")}));
 		msgdat.put("0100", new MessageType("0100", 0, "Rear Hub", new SubMessage[] {}));
-		msgdat.put("0110", new MessageType("0110", 6, "Front Hub", new SubMessage[] {new SubMessage("Brake Pressure 1", false, 0.7629395, "psi"), new SubMessage("Brake Pressure 0", false, 0.7629395, "psi"), new SubMessage("Steer. Angle", false, 0.0878906, "deg"), new SubMessage("Unused", true, 1, "")}));
-		msgdat.put("0200", new MessageType("0200", 8, "Motec", new SubMessage[] {new SubMessage("RPM", true, 1, "rpm"), new SubMessage("Throttle Position", true, 0.1, "%"), new SubMessage("Oil Pressure", true, 0.1, "psi"), new SubMessage("Oil Temperature", true, 0.1, "C")}));
-		msgdat.put("0201", new MessageType("0201", 8, "Motec", new SubMessage[] {new SubMessage("Engine Temperature", true, 0.1, "C"), new SubMessage("Lambda", true, 0.001, "La"), new SubMessage("Manifold Pressure", true, 0.1, "kPa"), new SubMessage("Battery Voltage", true, 0.01, "V")}));
-		msgdat.put("0202", new MessageType("0202", 8, "Motec", new SubMessage[] {new SubMessage("Wheel Speed FL", true, 0.1, "MPH"), new SubMessage("Wheel Speed FR", true, 0.1, "MPH"), new SubMessage("Wheel Speed RR", true, 0.1, "MPH"), new SubMessage("Wheel Speed RL", true, 0.1, "MPH")}));
-		msgdat.put("0203", new MessageType("0203", 2, "Motec", new SubMessage[] {new SubMessage("Ground Speed", true, 0.1, "MPH"), new SubMessage("Unused", true, 0.1, "")}));
+		msgdat.put("0110", new MessageType("0110", 6, "Front Hub", new SubMessage[] {new SubMessage("Brake Pressure 1", false, false, 0.7629395f, "psi"), new SubMessage("Brake Pressure 0", false, false, 0.7629395f, "psi"), new SubMessage("Steer. Angle", false, false, 0.0878906f, "deg"), new SubMessage("Unused", true, false, 1, "")}));
+		msgdat.put("0200", new MessageType("0200", 8, "Motec", new SubMessage[] {new SubMessage("RPM", true, true, 1, "rpm"), new SubMessage("Throttle Position", true, true, 0.1f, "%"), new SubMessage("Oil Pressure", true, true, 0.1f, "psi"), new SubMessage("Oil Temperature", true, true, 0.1f, "C")}));
+		msgdat.put("0201", new MessageType("0201", 8, "Motec", new SubMessage[] {new SubMessage("Engine Temperature", true, true, 0.1f, "C"), new SubMessage("Lambda", true, true, 0.001f, "La"), new SubMessage("Manifold Pressure", true, true, 0.1f, "kPa"), new SubMessage("Battery Voltage", true, true, 0.01f, "V")}));
+		msgdat.put("0202", new MessageType("0202", 8, "Motec", new SubMessage[] {new SubMessage("Wheel Speed FL", true, true, 0.1f, "MPH"), new SubMessage("Wheel Speed FR", true, true, 0.1f, "MPH"), new SubMessage("Wheel Speed RR", true, true, 0.1f, "MPH"), new SubMessage("Wheel Speed RL", true, true, 0.1f, "MPH")}));
+		msgdat.put("0203", new MessageType("0203", 2, "Motec", new SubMessage[] {new SubMessage("Ground Speed", true, true, 0.1f, "MPH"), new SubMessage("Unused", true, true, 0.1f, "")}));
 	}
 	
 	public void setInputFile(File file) {
--- src/SubMessage.java
+++ src/SubMessage.java
@@ -1,12 +1,14 @@
 public class SubMessage {
 	private String title;
 	private boolean isSigned;
+	private boolean isBigEndian;
 	private double scalar;
 	private String units;
 
-	public SubMessage(String title, boolean isSigned, double scalar, String units) {
+	public SubMessage(String title, boolean isSigned, boolean isBigEndian, float scalar, String units) {
 		this.title = title;
 		this.isSigned = isSigned;
+		this.isBigEndian = isBigEndian;
 		this.scalar = scalar;
 		this.units = units;
 	}
@@ -14,8 +16,17 @@ public class SubMessage {
 	public String translateData(byte one, byte two) {
 		String printString = "";
 		if(title.equals("Unused") || title.equals("Reserved")) return printString;
-		if(isSigned || !isSigned) {
-			printString = title + "," + (Integer.parseInt(hex(one) + hex(two), 16) * scalar) + "\n";
+		
+		String hexString = isBigEndian ? hex(one) + hex(two) : hex(two) + hex(one);
+		int hexValue = Integer.parseInt(hexString,16);
+		
+		if(title.equals("Yaw Rate")) hexValue -= 0x8000;
+		
+		if(isSigned) {
+			printString = title + "," + (((short)hexValue) * scalar) + "\n";
+		}
+		else {
+			printString = title + "," + (hexValue * scalar) + "\n";
 		}
 		return printString;
 	}
