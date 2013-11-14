58bca363e9ec6911b4f5af3324c0bcc1540dac79
Andrew Mass <amass1212@gmail.com>
2013-11-13 20:45:55 -0600

    Formats Runner class. Adds comments and documentation for Runner class.

--- src/Runner.java
+++ src/Runner.java
@@ -1,6 +1,20 @@
-
+/**
+ * Class which provides an entry point for the application. When the main() method is run, a new
+ * Converter object is instantiated.
+ * 
+ * @author Andrew Mass
+ * @version 1.0.0
+ */
 public class Runner {
-	public static void main(String[] args) {
-		new Converter();
-	}
+
+  /**
+   * Creates a new Converter object, which runs the Converter() constructor and starts the
+   * application.
+   * 
+   * @param args
+   *          Command-line arguments sent to the program. These are unused for this application.
+   */
+  public static void main(String[] args) {
+    new Converter();
+  }
 }
